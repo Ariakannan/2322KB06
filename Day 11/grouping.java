8e798ae02ac83be7a25f93307d3f1020c3427c53
Ariakannan <161226588+Ariakannan@users.noreply.github.com>
2025-06-28 18:49:09 +0530

    Delete Day 11/grouping.java

--- Day 11/grouping.java
+++
@@ -1,4 +0,0 @@
-create table sales(id int primary key,productid int,qty int,saledate date);
-insert into sales values(1,1,10,'2025-01-01'),(2,2,5,'2025-01-01'),(3,1,7,'2025-01-02'),(4,3,20,'2025-01-02');
-
-select productid,sum(qty) from sales group by productid;
